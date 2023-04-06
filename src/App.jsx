fa4894bf5f4a37b509128a83f3b0d93cec6ed27a
Alibek <alibekjumaniyazov007@gmail.com>
2023-04-07 00:27:38 +0500

    addd

--- src/App.jsx
+++ src/App.jsx
@@ -3,57 +3,90 @@ import { useEffect, useRef, useState } from "react";
 import Lists from "./Lists";
 
 function App() {
-  const [state ,setState] = useState([
 
+  const [karzinka, setKorazinka] = useState(JSON.parse(localStorage.getItem('add')) || [])
+
+
+  localStorage.setItem('add', JSON.stringify(karzinka))
+
+
+  const [state, setState] = useState([
     {
-      id:1,
-      name:"alibek",
-      familiya:"Jumaniyazov",
-      yosh:18
+      card:[],
+      id: 1,
+      name: "alibek",
+      familiya: "Jumaniyazov",
+      yosh: 18
     },
     {
-      id:2,
-      name:"amirbek",
-      familiya:"Jumaniyazov",
-      yosh:19
+      card:[],
+      id: 2,
+      name: "amirbek",
+      familiya: "Jumaniyazov",
+      yosh: 19
     },
-        {
-          id:3,
-      name:"akbar",
-      familiya:"Jumaniyazov",
-      yosh:20
+    {
+      card:[],
+      id: 3,
+      name: "akbar",
+      familiya: "Jumaniyazov",
+      yosh: 20
     },
-        {
-          id:4,
-      name:"Javohir",
-      familiya:"Jumaniyazov",
-      yosh:17
+    {
+      card:[],
+      id: 4,
+      name: "Javohir",
+      familiya: "Jumaniyazov",
+      yosh: 17
     }
   ])
-  function add(ee){
-    console.log(ee
-      );
-}
+  function add(id, name, familiya, yosh) {
+    setKorazinka(value => [...value, {
+      id: id,
+      name: name,
+      familiya: familiya,
+      yosh: yosh,
+    }])
+
+
+  }
+
+// useEffect(() =>{
+
+// }
+ 
+//   ,[state.card])
 
   return (
-    <div className="App"> 
+    <div className="App">
 
-    <div className="jad">
-      <p></p>
-      <p></p>
-      <p></p>
-    </div>
 
-    <div className="">
-      {
 
-        state.map((item) => (
-          <Lists item={item}/>
-          
-        ))
-      }
+      <div className="">
+        {
 
-    </div>
+          state.map((item) => (
+            <Lists item={item} add={add} />
+
+          ))
+        }
+
+      </div>
+
+
+      <div className="jad">
+        {
+          karzinka.map((item) => (
+            <div className="jad">
+              <p>{item.name}</p>
+              <p>{item.familiya}</p>
+              <p>{item.yosh}</p>
+            </div>
+          ))
+        }
+      </div>
+
+
     </div>
   );
 }
--- src/Lists.jsx
+++ src/Lists.jsx
@@ -1,10 +1,11 @@
 export default function Lists({item ,add}){
+
     return(
         <div className="LIsts">
             <h1>{item.name}</h1>
             <p>{item.familiya}</p>
             <span>{item.yosh}</span>
-            <button onClick={() =>  add(item.id)}>Jadvalga Qoshish</button>
+            <button onClick={() =>  add(item.id , item.name ,item.familiya , item.yosh)}>Jadvalga Qoshish</button>
         </div>
     )
 }
